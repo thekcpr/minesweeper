8f98e75e1cde2f980c44abcd7df810452953dfac
Kacper Jasiński <kacper_jasinski@outlook.com>
2025-04-23 17:07:28 +0200

    style(view): updated beveled border rendering in ClassicTheme

--- src/view/classic_theme.py
+++ src/view/classic_theme.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.gfxdraw
 import json
 from .base_theme import Theme
 from .sprites import ThreeDigitDisplay, SmileFace
@@ -12,25 +13,29 @@ class ClassicTheme(Theme):
 
         # Background variables
         self.ui_scale = 2
-        self.beavel = 3 * self.ui_scale
+        self.beavel_thickness = 3
+        self.beavel = self.beavel_thickness * self.ui_scale
         self.border = 6 * self.ui_scale
 
 
         # self.menu_bar_height = 18 * self.ui_scale
         self.menu_bar_height = 0
-        self.info_beavel = 2 * self.ui_scale
-        self.digit_beavel = 1 * self.ui_scale
+        self.info_beavel_thickness = 2
+        self.info_beavel = self.info_beavel_thickness * self.ui_scale
+        self.digit_beavel_thickness = 1
+        self.digit_beavel = self.digit_beavel_thickness * self.ui_scale
         self.digit_width = 13 * self.ui_scale
         self.digit_height = 23 * self.ui_scale
         self.info_height = (2 * 4) * self.ui_scale + self.info_beavel + self.digit_height  # (4 + 1 + 23 + 1 + 4)
 
         self.tile_size = 16 * self.ui_scale
 
-        self.screen_width = 2 * self.border + 4 * self.beavel + self.tile_size * self.columns + 1
-        self.screen_height = self.menu_bar_height + 3 * self.border + 4 * self.beavel + 2 * self.info_beavel + self.info_height + self.tile_size * self.rows + 1
+        self.screen_width = 2 * self.border + 4 * self.beavel + self.tile_size * self.columns
+        self.screen_height = self.menu_bar_height + 3 * self.border + 4 * self.beavel + 2 * self.info_beavel + self.info_height + self.tile_size * self.rows
 
-        self.board_topleft = (2 * self.beavel + self.border + 1,
-                              self.menu_bar_height + 2 * self.beavel + 2 * self.border + 2 * self.info_beavel + self.info_height + 1)
+        self.board_topleft = (self.beavel + self.border + self.beavel,
+                              self.menu_bar_height + 2 * self.beavel + 2 * self.border + 2 * self.info_beavel + self.info_height)
+        print(self.board_topleft)
         self.digits_bomb_topleft = (self.beavel + 2  * self.border + self.info_beavel + self.digit_beavel,
                                     self.menu_bar_height + self.beavel + self.border + self.info_beavel + 4 * self.ui_scale + self.digit_beavel)
         self.digits_time_topleft = (self.screen_width - self.beavel - 2 * self.border - self.info_beavel - 2 * self.digit_beavel - 3 * self.digit_width,
@@ -89,15 +94,6 @@ class ClassicTheme(Theme):
         self.sounds['win'].set_volume(0.1)
         self.sounds['lose'].set_volume(0.1)
 
-        # self.menu_icons = {'beginner_active': pygame. transform.scale_by(pygame.image.load('images/menu/beginner_active.png').convert(), self.ui_scale),
-        #                    'beginner_inactive': pygame. transform.scale_by(pygame.image.load('images/menu/beginner_inactive.png').convert(), self.ui_scale),
-        #                    'intermediate_active': pygame. transform.scale_by(pygame.image.load('images/menu/intermediate_active.png').convert(), self.ui_scale),
-        #                    'intermediate_inactive': pygame. transform.scale_by(pygame.image.load('images/menu/intermediate_inactive.png').convert(), self.ui_scale),
-        #                    'expert_active': pygame. transform.scale_by(pygame.image.load('images/menu/expert_active.png').convert(), self.ui_scale),
-        #                    'expert_inactive': pygame. transform.scale_by(pygame.image.load('images/menu/expert_inactive.png').convert(), self.ui_scale),
-        #                    'custom_active': pygame. transform.scale_by(pygame.image.load('images/menu/custom_active.png').convert(), self.ui_scale),
-        #                    'custom_inactive': pygame. transform.scale_by(pygame.image.load('images/menu/custom_inactive.png').convert(), self.ui_scale)}
-
     # endregion
 
 
@@ -137,67 +133,117 @@ class ClassicTheme(Theme):
         self.background = pygame.Surface((self.screen_width, self.screen_height))
         self.background.fill(self.neutral_color)
 
-        # Menu bar
-        # pygame.draw.rect(self.screen, 'White', [(0, 0), (self.screen_width, self.menu_bar_height - 1)])
-
         # Border around the screen
         topleft = (0, self.menu_bar_height)
         bottomright = (self.screen_width, self.screen_height)
-        self.draw_bevaled_border(self.background, topleft, bottomright, self.beavel, 'outset')
+        self.draw_bevaled_border(self.background, topleft, bottomright, self.beavel_thickness, 'outset')
 
         # Border around info bar
         topleft = (self.beavel + self.border,
                    self.menu_bar_height + self.beavel + self.border)
         bottomright = (self.screen_width - self.beavel - self.border,
                        self.menu_bar_height + self.beavel + self.border + 2 * self.info_beavel + self.info_height)
-        self.draw_bevaled_border(self.background, topleft, bottomright, self.info_beavel, 'inset')
+        self.draw_bevaled_border(self.background, topleft, bottomright, self.info_beavel_thickness, 'inset')
 
         # Border around tile board
         topleft = (self.beavel + self.border,
                    self.menu_bar_height + self.beavel + 2 * self.border + 2 * self.info_beavel + self.info_height)
         bottomright = (self.screen_width - self.beavel - self.border,
                        self.screen_height - self.beavel - self.border)
-        self.draw_bevaled_border(self.background, topleft, bottomright, self.beavel, 'inset')
+        self.draw_bevaled_border(self.background, topleft, bottomright, self.beavel_thickness, 'inset')
 
         # Border around bomb counter
         topleft = (self.digits_bomb_topleft[0] - self.digit_beavel,
                    self.digits_bomb_topleft[1] - self.digit_beavel)
         bottomright = (topleft[0] + 3 * self.digit_width + 2 * self.digit_beavel,
                        topleft[1] + 2 * self.digit_beavel + self.digit_height)
-        self.draw_bevaled_border(self.background, topleft, bottomright, self.digit_beavel, 'inset')
+        self.draw_bevaled_border(self.background, topleft, bottomright, self.digit_beavel_thickness, 'inset')
 
         # Border around timer
         topleft = (self.digits_time_topleft[0] - self.digit_beavel,
                    self.digits_time_topleft[1] - self.digit_beavel)
         bottomright = (topleft[0] + 3 * self.digit_width + 2 * self.digit_beavel,
                        topleft[1] + 2 * self.digit_beavel + self.digit_height)
-        self.draw_bevaled_border(self.background, topleft, bottomright, self.digit_beavel, 'inset')
+        self.draw_bevaled_border(self.background, topleft, bottomright, self.digit_beavel_thickness, 'inset')
 
  
-    def draw_bevaled_border(self, screen: pygame.surface.Surface, topleft: tuple[int, int], bottomright: tuple[int, int], thickness: int, type: str) -> None:
+    def draw_bevaled_border(self, screen: pygame.surface.Surface, topleft: tuple[int, int], bottomright: tuple[int, int], bevel_thickness: int, type: str) -> None:
         if type == 'outset': color = (self.highlight_color, self.shadow_color)
         if type == 'inset': color = (self.shadow_color, self.highlight_color)
-        points = {'topleft': (topleft[0], topleft[1]),
-                  'inner_topleft': (topleft[0] + thickness, topleft[1] + thickness),
-                  'topright': (bottomright[0], topleft[1]),
-                  'inner_topright': (bottomright[0] - thickness, topleft[1] + thickness),
-                  'bottomright': (bottomright[0], bottomright[1]),
-                  'inner_bottomright': (bottomright[0] - thickness, bottomright[1] - thickness),
-                  'bottomleft': (topleft[0], bottomright[1]), 
-                  'inner_bottomleft': (topleft[0] + thickness, bottomright[1] - thickness)}
-        pygame.draw.polygon(screen, color[0], [points['topleft'],
-                                                    points['bottomleft'],
-                                                    points['inner_bottomleft'],
-                                                    points['inner_topleft'],
-                                                    points['inner_topright'],
-                                                    points['topright']])
-        pygame.draw.polygon(screen, color[1], [points['bottomright'],
-                                                    points['topright'],
-                                                    points['inner_topright'],
-                                                    points['inner_bottomright'],
-                                                    points['inner_bottomleft'],
-                                                    points['bottomleft']])
-        del points
+        
+        for i in range(bevel_thickness):
+            topleft_point = (topleft[0] + i * self.ui_scale, topleft[1] + i * self.ui_scale)
+            pygame.draw.line(screen, color[0], (topleft_point[0], topleft_point[1]), (topleft_point[0], bottomright[1] - (i + 1) * self.ui_scale - self.ui_scale / 2), self.ui_scale)
+            pygame.draw.line(screen, color[0], (topleft_point[0], topleft_point[1]), (bottomright[0] - (i + 1) * self.ui_scale - self.ui_scale / 2, topleft_point[1]), self.ui_scale)
+
+            bottomright_point = (bottomright[0] - (i + 1) * self.ui_scale, bottomright[1] - (i + 1) * self.ui_scale)
+            pygame.draw.line(screen, color[1], (bottomright_point[0], bottomright_point[1] + self.ui_scale / 2), (bottomright_point[0], topleft[1] + (i + 1) * self.ui_scale), self.ui_scale)
+            pygame.draw.line(screen, color[1], (bottomright_point[0] + self.ui_scale / 2, bottomright_point[1]), (topleft[0] + (i + 1) * self.ui_scale, bottomright_point[1]), self.ui_scale)
+
+
+        # thickness = bevel_thickness * self.ui_scale
+        # points = {'topleft': (topleft[0], topleft[1]),
+        #           'inner_topleft': (topleft[0] + thickness, topleft[1] + thickness),
+        #           'topright': (bottomright[0], topleft[1]),
+        #           'inner_topright': (bottomright[0] - thickness, topleft[1] + thickness),
+        #           'bottomright': (bottomright[0], bottomright[1]),
+        #           'inner_bottomright': (bottomright[0] - thickness, bottomright[1] - thickness),
+        #           'bottomleft': (topleft[0], bottomright[1]), 
+        #           'inner_bottomleft': (topleft[0] + thickness, bottomright[1] - thickness)}
+        
+        # first_bevel_points = [points['topleft'], points['bottomleft']]
+        # second_bevel_points = [points['bottomright'], points['topright']]
+
+        # for i in range(bevel_thickness):
+        #     point = (first_bevel_points[-1][0], first_bevel_points[-1][1] - self.ui_scale)
+        #     first_bevel_points.append(point)
+        #     point = (first_bevel_points[-1][0] + self.ui_scale, first_bevel_points[-1][1])
+        #     first_bevel_points.append(point)
+
+        #     point = (second_bevel_points[-1][0], second_bevel_points[-1][1] + self.ui_scale)
+        #     second_bevel_points.append(point)
+        #     point = (second_bevel_points[-1][0] - self.ui_scale, second_bevel_points[-1][1])
+        #     second_bevel_points.append(point)
+
+        # first_bevel_points.append(points['inner_topleft'])
+        # first_bevel_points.append(points['inner_topright'])
+        # second_bevel_points.append(points['inner_bottomright'])
+        # second_bevel_points.append(points['inner_bottomleft'])
+
+        # for i in range(bevel_thickness):
+        #     point = (first_bevel_points[-1][0], first_bevel_points[-1][1] - self.ui_scale)
+        #     first_bevel_points.append(point)
+        #     point = (first_bevel_points[-1][0] + self.ui_scale, first_bevel_points[-1][1])
+        #     first_bevel_points.append(point)
+
+        #     point = (second_bevel_points[-1][0], second_bevel_points[-1][1] + self.ui_scale)
+        #     second_bevel_points.append(point)
+        #     point = (second_bevel_points[-1][0] - self.ui_scale, second_bevel_points[-1][1])
+        #     second_bevel_points.append(point)
+
+        # first_bevel_points.pop(1)
+        # first_bevel_points.pop(-1)
+        # second_bevel_points.pop(1)
+        # second_bevel_points.pop(-1)
+
+        # print(f"1st: {first_bevel_points}")
+        # print(f"2nd: {second_bevel_points}")
+        # pygame.gfxdraw.filled_polygon(screen, first_bevel_points, color[0])
+        # pygame.gfxdraw.filled_polygon(screen, second_bevel_points, color[1])
+
+        # pygame.draw.polygon(screen, color[0], [points['topleft'],
+        #                                             points['bottomleft'],
+        #                                             points['inner_bottomleft'],
+        #                                             points['inner_topleft'],
+        #                                             points['inner_topright'],
+        #                                             points['topright']])
+        # pygame.draw.polygon(screen, color[1], [points['bottomright'],
+        #                                             points['topright'],
+        #                                             points['inner_topright'],
+        #                                             points['inner_bottomright'],
+        #                                             points['inner_bottomleft'],
+        #                                             points['bottomleft']])
+        # del points
 
 # endregion
 
