b5af78e656f1b4415db0cf7566cc11cf1a3c4218
Kacper Jasiński <kacper_jasinski@outlook.com>
2025-04-23 13:17:27 +0200

    refactor(controller): split main_loop into event handlers

--- src/controller.py
+++ src/controller.py
@@ -37,127 +37,26 @@ class GameController:
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
-
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-                    self.style = 'win31'
-                    self.update_theme(self.style)
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_2:
-                    self.style = 'win95'
-                    self.update_theme(self.style)
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_3:
-                    self.style = 'winxp'
-                    self.update_theme(self.style)
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_4:
-                    self.style = 'mono'
-                    self.update_theme(self.style)
-
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_b:
-                    self.rows = 9
-                    self.cols = 9
-                    self.bombs = 10
-                    self.display_init()
-                    self.new_game()
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_i:
-                    self.rows = 16
-                    self.cols = 16
-                    self.bombs = 40
-                    self.display_init()
-                    self.new_game()
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
-                    self.rows = 16
-                    self.cols = 30
-                    self.bombs = 99
-                    self.display_init()
-                    self.new_game()
+                    self.handle_quit_event()
 
                 if event.type == self.second and self.is_game_active and self.is_first_move == False:
                     self.gui.play_tick_sound()
 
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    self.is_button_pressed = True
-                    if self.is_game_active:
-                        self.gui.update_face_sprite('faceooh')
-                    mouse_pos = pygame.mouse.get_pos()
-                    tile_under_mouse = self.get_tile_under_mouse(mouse_pos)
+                if event.type == pygame.KEYDOWN:
+                    self.handle_heyboard_event(event)
 
-                    # Restart button press down animation
-                    if self.face and self.face.rect.collidepoint(event.pos):
-                        self.gui.update_face_sprite('opensmile')
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_mouse_button_down(event)
 
-                if event.type == pygame.MOUSEMOTION and self.is_button_pressed:
-                    if not self.face.rect.collidepoint(event.pos):
-                        self.gui.update_face_sprite(self.face_state)  # przestań wyglądać na wciśniętą
-                    else:
-                        self.gui.update_face_sprite('opensmile')  # nadal wciśnięta
-                    if self.get_tile_under_mouse(event.pos) and self.is_game_active:
-                        self.gui.update_face_sprite('faceooh')
+                if event.type == pygame.MOUSEMOTION:
+                    self.handle_mouse_motion(event)
 
                 if event.type == pygame.MOUSEBUTTONUP:
-                    self.is_button_pressed = False
-                    self.gui.update_face_sprite(self.face_state)
-                    mouse_pos = pygame.mouse.get_pos()
-                    tile_under_mouse = self.get_tile_under_mouse(mouse_pos)
-
-                    # Restart button click
-                    if self.face and self.face.rect.collidepoint(event.pos):
-                        self.new_game()
-
-            # Game State Events
-                    if not self.is_game_active: break
-
-                    # Tile interaction
-                    tile_under_mouse = self.get_tile_under_mouse(mouse_pos)
-
-                    # Reveal (Left) click
-                    if tile_under_mouse and event.button == 1:
-
-                        if self.is_first_move:
-                            self.round_start_time = pygame.time.get_ticks()
-                            self.is_first_move = False
-                            self.board.generate_board(tile_under_mouse)
-
-                        self.board.reveal(tile_under_mouse)
-                        self.draw_tiles(self.board.get_board(), self.board.get_tiles_to_update())
-
-                        # Solved condition
-                        if self.board.is_solved():
-                            self.is_game_active = False
-                            self.gui.play_win_sound()
-                            self.face_state = 'facewin'
-                            self.gui.update_face_sprite(self.face_state)
-                            self.board.flag_remaining_tiles()
-                            self.draw_tiles(self.board.get_board(), self.board.get_tiles_to_update())
-
-                        # Failed condition
-                        if self.board.is_failed():
-                            self.is_game_active = False
-                            self.gui.play_lose_sound()
-                            self.face_state = 'facedead'
-                            self.gui.update_face_sprite(self.face_state)
-
-                    # Flag (Right) click
-                    elif tile_under_mouse and event.button == 3:
-                        self.board.handle_right_click(tile_under_mouse)
-                        self.draw_tiles(self.board.get_board(), self.board.get_tiles_to_update())
-
-            if self.is_game_active and self.is_button_pressed:
-                current_hover = self.get_tile_under_mouse(pygame.mouse.get_pos())
-                if current_hover != self.hover_tile:
-                    if self.hover_tile:
-                        self.tiles[self.hover_tile].draw(self.screen)
-                    if current_hover:
-                        self.tiles[current_hover].draw_hover(self.screen)
-                    
-                    self.hover_tile = current_hover
-
-
-            # Updates unflagged bomb counter and timer
-            if self.is_game_active and not self.is_first_move:
-                self.time = (pygame.time.get_ticks() - self.round_start_time)//1000
-                self.gui.update_timer(self.time)
-            self.gui.update_mines_couter(self.board.get_unflagged_bombs_num())
+                    self.handle_mouse_button_up(event)
+            
+            if self.is_game_active:
+                self.handle_tile_hover_draw()
+                self.handle_game_state()
                 
 
             pygame.display.update()
@@ -207,6 +106,8 @@ class GameController:
         self.gui.build_info_sprites()
         self.gui.update_face_sprite(self.face_state)
         self.draw_board(self.board.get_board(), self.board.get_all_tiles())
+        self.gui.update_mines_couter(self.board.get_unflagged_bombs_num())
+        self.gui.update_timer(self.time)
 
 
     def draw_board(self, board: dict[tuple[int, int]: dict[str: str, str: str]], all_tiles: list[tuple[int, int]]):
@@ -225,3 +126,136 @@ class GameController:
         for position in tiles_to_update:
             self.tiles[position].update(board[position]['state'], board[position]['content'])
             self.tiles[position].draw(self.screen)
+
+
+    def handle_quit_event(self):
+        pygame.quit()
+        exit()
+
+
+    def handle_heyboard_event(self, event):
+        if event.key == pygame.K_1:
+            self.style = 'win31'
+            self.update_theme(self.style)
+        if event.key == pygame.K_2:
+            self.style = 'win95'
+            self.update_theme(self.style)
+        if event.key == pygame.K_3:
+            self.style = 'winxp'
+            self.update_theme(self.style)
+        if event.key == pygame.K_4:
+            self.style = 'mono'
+            self.update_theme(self.style)
+
+        if event.key == pygame.K_b:
+            self.rows = 9
+            self.cols = 9
+            self.bombs = 10
+            self.display_init()
+            self.new_game()
+        if event.key == pygame.K_i:
+            self.rows = 16
+            self.cols = 16
+            self.bombs = 40
+            self.display_init()
+            self.new_game()
+        if event.key == pygame.K_e:
+            self.rows = 16
+            self.cols = 30
+            self.bombs = 99
+            self.display_init()
+            self.new_game()
+
+
+    def handle_mouse_button_down(self, event):
+        if event.button == 1:
+            self.is_button_pressed = True
+
+            if self.is_game_active:
+                self.gui.update_face_sprite('faceooh')
+
+            mouse_pos = pygame.mouse.get_pos()
+            tile_under_mouse = self.get_tile_under_mouse(mouse_pos)
+
+            # Restart button press down animation
+            if self.face and self.face.rect.collidepoint(event.pos):
+                self.gui.update_face_sprite('opensmile')
+
+
+    def handle_mouse_motion(self, event):
+        if self.is_button_pressed:
+            if not self.face.rect.collidepoint(event.pos):
+                self.gui.update_face_sprite(self.face_state)
+            else:
+                self.gui.update_face_sprite('opensmile')
+            if self.get_tile_under_mouse(event.pos) and self.is_game_active:
+                self.gui.update_face_sprite('faceooh')
+
+
+    def handle_mouse_button_up(self, event):
+        self.is_button_pressed = False
+        self.gui.update_face_sprite(self.face_state)
+
+        mouse_pos = pygame.mouse.get_pos()
+        tile_under_mouse = self.get_tile_under_mouse(mouse_pos)
+
+        # Restart button click
+        if self.face and self.face.rect.collidepoint(event.pos):
+            self.new_game()
+
+        # Game State Events
+        if self.is_game_active:
+
+            # Handle Right click
+            if tile_under_mouse and event.button == 3:
+                self.board.handle_right_click(tile_under_mouse)
+                self.draw_tiles(self.board.get_board(), self.board.get_tiles_to_update())
+
+            # Handle Left click
+            elif tile_under_mouse and event.button == 1:
+
+                if self.is_first_move:
+                    self.round_start_time = pygame.time.get_ticks()
+                    self.is_first_move = False
+                    self.board.generate_board(tile_under_mouse)
+
+                self.board.reveal(tile_under_mouse)
+
+                # Solved condition
+                if self.board.is_solved():
+                    self.is_game_active = False
+                    self.gui.play_win_sound()
+                    self.face_state = 'facewin'
+                    self.gui.update_face_sprite(self.face_state)
+                    self.board.flag_remaining_tiles()
+                    self.gui.update_mines_couter(self.board.get_unflagged_bombs_num())
+                    print(f'Time: {self.time_detailed/100}')
+
+                # Failed condition
+                elif self.board.is_failed():
+                    self.is_game_active = False
+                    self.gui.play_lose_sound()
+                    self.face_state = 'facedead'
+                    self.gui.update_face_sprite(self.face_state)
+
+                self.draw_tiles(self.board.get_board(), self.board.get_tiles_to_update())
+
+
+    def handle_tile_hover_draw(self):
+        if self.is_button_pressed:
+            current_hover = self.get_tile_under_mouse(pygame.mouse.get_pos())
+            if current_hover != self.hover_tile:
+                if self.hover_tile:
+                    self.tiles[self.hover_tile].draw(self.screen)
+                if current_hover:
+                    self.tiles[current_hover].draw_hover(self.screen)
+                
+                self.hover_tile = current_hover
+
+
+    def handle_game_state(self):
+        if not self.is_first_move:
+            self.time_detailed = (pygame.time.get_ticks() - self.round_start_time)//10
+            self.time = self.time_detailed//100
+            self.gui.update_timer(self.time)
+        self.gui.update_mines_couter(self.board.get_unflagged_bombs_num())
